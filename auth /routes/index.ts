0ad6f537fd69a6c153bce3bb07ea7a0ab5686cbe
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2024-03-14 10:47:54 +0500

    auth finished
    auth finished

--- auth /routes/index.ts
+++ auth /routes/index.ts
@@ -1,12 +1,68 @@
 import { Response, Router } from "express";
 import userModel from "../models";
 import jwt from "jsonwebtoken";
+import querystring from "querystring";
 
 const const_rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 const client_id =
   "604927976478-4khcpll4gvva7cdot4tq0f5hi9v9qen6.apps.googleusercontent.com";
+const client_secret = "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";
+const redirect_uri = "http://localhost:8080/auth/google/callback";
+
 const router = Router();
-router.post("/login/google", (req, res) => {
+
+const getTokens = async ({
+  code,
+}: {
+  code: string;
+}): Promise<{
+  access_token: string;
+  expires_in: number;
+  id_token: string;
+}> => {
+  const url = "https://oauth2.googleapis.com/token";
+  const values = {
+    code,
+    client_id,
+    client_secret,
+    redirect_uri,
+    grant_type: "authorization_code",
+  };
+  const data = await (
+    await fetch(url, {
+      method: "POST",
+      body: querystring.stringify(values),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+  ).json();
+  return data as Promise<{
+    access_token: string;
+    expires_in: number;
+    id_token: string;
+  }>;
+};
+router.get("/auth/google/callback", async (req, res) => {
+  const code = req.query.code as string;
+  const { id_token, access_token } = await getTokens({ code });
+
+  const data = await (
+    await fetch(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${id_token}`,
+        },
+      },
+    )
+  ).json();
+  res.status(200).json({
+    message: "Success",
+    data,
+  });
+});
+router.get("/login/google", (req, res) => {
   const options = {
     redirect_uri: "http://localhost:8080/auth/google/callback",
     client_id,
@@ -19,7 +75,7 @@ router.post("/login/google", (req, res) => {
     ].join(" "),
   };
   return res.redirect(
-    `${const_rootUrl}?redirect_url=${options.redirect_uri}&client_id=${options.client_id}&access_type=${options.access_type}&response_type=${options.response_type}&prompt=${options.prompt}&scope=${options.scope}`,
+    `${const_rootUrl}?redirect_uri=${options.redirect_uri}&client_id=${options.client_id}&access_type=${options.access_type}&response_type=${options.response_type}&prompt=${options.prompt}&scope=${options.scope}`,
   );
 });
 
